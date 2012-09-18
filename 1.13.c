e402c5d102f496c8c8a11fe27284cb0fb19d0d09
Sasha Laundy <sasha.laundy@gmail.com>
2012-09-18 00:00:02 -0400

    Set up the files for next time

---
+++ 1.13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* 1-13 print a histogram of the lengths of words in input
+   it's easy to draw histogram with the bars horizontal
+   a vertical orientation is more challenging. */
+
+main() {
+
+}
---
+++ 1.14.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* 1-14 Write a program to print a histogram of the frequencies of different characters in its input */
+
+main() {
+
+}
