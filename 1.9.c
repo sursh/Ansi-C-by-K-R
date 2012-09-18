a87d820d0c494cc8bfd3a2a8a561b75b4b8b2eed
Sasha Laundy <sasha.laundy@gmail.com>
2012-09-17 23:01:35 -0400

    pseudocode

---
+++ 1.9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* write a program to copy its input to its output, 
+   replacing each string of one or more blanks by a single blank */
+
+main() {
+
+  int c, isSpace;
+  c = isSpace = 0;
+
+  while ((c = getchar()) != EOF) {
+    get char
+    if !Space 
+      put char
+    else if (flag off)
+      putchar 
+      switch flag
+    else /*the flag is off */
+      ;
+  }
+}
