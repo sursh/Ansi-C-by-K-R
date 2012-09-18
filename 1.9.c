7e756a7aca18abf9eef2cde17d07106550082800
Sasha Laundy <sasha.laundy@gmail.com>
2012-09-17 23:07:15 -0400

    Finish it up

--- 1.9.c
+++ 1.9.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
 /* write a program to copy its input to its output, 
-   replacing each string of one or more blanks by a single blank */
+   replacing each string of one or more blanks by a single blank 
+
+   without using bools
+
+   */
 
 main() {
 
-  int c, isSpace;
-  c = isSpace = 0;
+  int c, alreadySpace;
+  c = alreadySpace = 0;
 
   while ((c = getchar()) != EOF) {
-    get char
-    if !Space 
-      put char
-    else if (flag off)
-      putchar 
-      switch flag
-    else /*the flag is off */
+    if (c != ' ') {
+      putchar(c);
+      alreadySpace = 0;
+    }
+    else if (alreadySpace == 0){
+      putchar(c);
+      alreadySpace = 1;
+    }
+    else
       ;
   }
 }
