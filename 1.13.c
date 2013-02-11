b9874c4509ec4ab7324dcd2694d0b634d3e8e40e
Sasha Laundy <sasha.laundy@gmail.com>
2013-02-11 17:54:05 -0500

    Fix histogram printing

--- 1.13.c
+++ 1.13.c
@@ -4,23 +4,16 @@
    it's easy to draw histogram with the bars horizontal
    a vertical orientation is more challenging. 
 
-  pseudocode: 
-
-  while the file isn't over
-    while you're not in whitespace (aka in a word)
-      tally up how many letters you've gone through
-    then, once you've hit whitespace, store the tally in the array. 
-
    */
 
-main() {
+int main() {
 
   #define MAX_WORD_LENGTH 20  
   #define YES 1
   #define NO  0
 
   int c, i, letterTally, inASpace;
-  int letterCounts[MAX_WORD_LENGTH];
+  int letterCounts[MAX_WORD_LENGTH] = {0};
 
   letterTally = i = 0;
   c = inASpace = NO;
@@ -34,7 +27,6 @@ main() {
     /* hitting the first whitespace after a word */
     else if (inASpace == NO){
       inASpace = YES;
-      printf("One more word with %d letters \n", letterTally);
       ++letterCounts[letterTally];
       letterTally = 0;
     }
@@ -44,8 +36,10 @@ main() {
   }
   
   /* print out the histogram */
-  for (i = 0; i < MAX_WORD_LENGTH; ++i)
+  for (i = 1; i < MAX_WORD_LENGTH; ++i)
     printf("%6d words with %d letters.\n", letterCounts[i], i);
+
+  return 1;
 
 }
 
