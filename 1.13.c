2e5126f7c012e458872cc8f0a8916c7931b713d1
Sasha Laundy <sasha.laundy@gmail.com>
2013-02-13 13:06:53 -0500

    Use bools

--- 1.13.c
+++ 1.13.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 /* 1-13 print a histogram of the lengths of words in input
    it's easy to draw histogram with the bars horizontal
    a vertical orientation is more challenging. */
 
-int main() {
+#define MAX_WORD_LENGTH 20  
 
-  #define MAX_WORD_LENGTH 20  
-  #define YES 1
-  #define NO  0
+int main() {
 
   int c, i, letterTally, inASpace, printed0s;
   int letterCounts[MAX_WORD_LENGTH + 1] = {0};
-  c = i = letterTally, printed0s = 0;
-  inASpace = NO;
+  c = 0;
+  i = 0;
+  letterTally = 0;
+  printed0s = 0;
+  inASpace = false;
 
   while ((c = getchar()) != EOF) {
 
     /* you are in a word */
     if (c != ' ' && c != '\t' && c != '\n') {
       ++letterTally;
-      inASpace = NO;
+      inASpace = false;
     }
 
     /* hitting the first whitespace after a word - add tally to array and reset for next word */
-    else if (inASpace == NO){
-      inASpace = YES;
+    else if (inASpace == false){
+      inASpace = true;
       ++letterCounts[letterTally];
       letterTally = 0;
     }
 
     else
-      /* it's a second space, ignore it */
+      /* it's a second space, igfalsere it */
       ;
   }
   
@@ -40,7 +42,7 @@ int main() {
   for (i = 1; i <= MAX_WORD_LENGTH; ++i)
     printf("%3d", i);
 
-  /* print vertical histogram as long as there are no blank rows */
+  /* print vertical histogram as long as there are false blank rows */
   while(printed0s < MAX_WORD_LENGTH){
 
     putchar('\n');
@@ -60,7 +62,7 @@ int main() {
   }
 
   putchar('\n');
-  return 1;
+  return 0;
 
 }
 
